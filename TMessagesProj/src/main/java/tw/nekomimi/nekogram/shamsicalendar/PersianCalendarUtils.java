f64237d03a4c5c2e0c20d101fca5f6898615886d
herotux <thefreetux@gmail.com>
2021-04-04 21:52:16 +0430

    fix shamsi date bug

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/shamsicalendar/PersianCalendarUtils.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/shamsicalendar/PersianCalendarUtils.java
@@ -16,7 +16,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package ir.cafebabe.calendar.utils;
+package tw.nekomimi.nekogram.shamsicalendar;
 
 /**
  * algorithms for converting Julian days to the Persian calendar, and vice versa are adopted
